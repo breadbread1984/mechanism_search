66022dc4a51808fdf47ecc2e464277417c40f313
breadbread1984 <breadbread1984@163.com>
2026-01-22 11:04:55 +0800

    update

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
 
 from absl import flags, app
-import numpy as np
-import fitz # pymupdf
 from mechanism_finder import MechanismFinder
+import cv2
 
 FLAGS = flags.FLAGS
 
@@ -12,16 +11,11 @@ def add_options():
 
 def main(unused_argv):
   mf = MechanismFinder()
-  pdf = fitz.open(FLAGS.input_pdf)
-  for idx, page in enumerate(pdf):
-    pix = page.get_pixmap(dpi=100)
-    img = np.frombuffer(pix.samples, dtype = np.uint8).reshape(pix.height, pix.width, -1)
-    if pix.n == 1: img = img[:,:,0]
-    elif pix.n == 3: img = img.reshape(pix.height, pix.width, 3)
-    elif pix.n == 4: img = img.reshape(pix.height, pix.width, 4)
-    results = mf.process(img)
-    if results['has_mechanism'] == True:
-      print(f"figure #{results['figure']}{'' if results['subfigure'] is None else results['subfigure']} on page {idx + 1} is mechanism diagram")
+  results = mf.process_pdf(FLAGS.input_pdf)
+  for result in results:
+    print(f"figure #{result['figure_num']} on page {result['page_num']} at position {result['position']} is a mechanism diagram")
+    cv2.imshow('', result['image'])
+    cv2.waitKey()
 
 if __name__ == "__main__":
   add_options()
--- mechanism_finder.py
+++ mechanism_finder.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 
-from typing import Optional, Literal
+from typing import Optional, Literal, Tuple
 from pydantic import BaseModel, Field
 from langchain_core.output_parsers import JsonOutputParser
+import numpy as np
+import fitz # pymupdf
 from prompt import Prompt
 from messages import *
 from models import *
@@ -15,6 +17,7 @@ class MechanismFinder(object):
       has_mechanism: bool = Field(..., description = "whether current picture contains a drug mechaism diagram.")
       figure: Optional[int] = Field(None, description = "figure number in integer format (if has_mechanism=true)")
       subfigure: Optional[Literal['a','b','c','d','e','f','g','h','i','j','k','l','m','n']] = Field(None, description = "subfigure number if appliable (is has_mechanism=true)")
+      position: Optional[Tuple[int,int,int,int]] = Field(None, description = "position (top left x1,y1, bottom right x2,y2) of the figure or subfigure in sequence x1,y1,x2,y2")
     self.parser = JsonOutputParser(pydantic_object = Output)
     self.instruction = self.parser.get_format_instructions()
     self.instruction = self.instruction.replace('{', '{{')
@@ -42,8 +45,27 @@ Please determine whether the current paper snippet contains a drug mechanism dia
 
 {self.instruction}""", image = image)
     ])
-  def process(self, image):
+  def process_image(self, image):
     prompt = self.get_prompt(image)
     output = self.model.inference(prompt)
     results = self.parser.parse(output)
     return results
+  def process_pdf(self, pdf_path):
+    pdf = fitz.open(pdf_path)
+    pics = list()
+    for idx, page in enumerate(pdf):
+      pix = page.get_pixmap(dpi=100)
+      img = np.frombuffer(pix.samples, dtype = np.uint8).reshape(pix.height, pix.width, -1)
+      if pix.n == 1: img = img[:,:,0]
+      elif pix.n == 3: img = img.reshape(pix.height, pix.width, 3)
+      elif pix.n == 4: img = img.reshape(pix.height, pix.width, 4)
+      results = self.process_image(img)
+      if results['has_mechanism'] == True:
+        x1,y1,x2,y2 = results['position']
+        pics.append({
+          'page_num': idx + 1,
+          'figure_num': results['figure'] + '' if results['subfigure'] is None else results['subfigure'],
+          'position': results['position'],
+          'image': img[y1:y2,x1:x2],
+        })
+    return pics
